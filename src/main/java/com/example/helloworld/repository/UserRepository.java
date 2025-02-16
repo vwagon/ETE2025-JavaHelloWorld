441cd43d51d88ca5568134e0c9c44b6d4184df93
Valentin <dmtvalentin@gmail.com>
2025-02-16 22:10:47 +0100

    Redeploiement complet de l'application java

--- src/main/java/com/example/helloworld/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.helloworld.repository;
-
-import com.example.helloworld.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
